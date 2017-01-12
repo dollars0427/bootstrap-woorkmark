798ad1a287cf50525a3eb1762a5a1ff18feef734
Sardo Ip <sardo@pixelactionstudio.com>
2017-01-12 11:11:29 +0800

    Use windows load to aviod preload issues

--- js/active.js
+++ js/active.js
@@ -1,7 +1,8 @@
 (function ($) {
-  $(document).ready(function(){
+  $(window).load(function(){
     $('.active-wookmark').wookmark({
-      itemWidth: 330,
+      itemWidth: 400,
+      offset: 5,
       align: 'left'
     });
   });
